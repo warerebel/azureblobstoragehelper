e88b07771a196f6063057c42e5c33bbc822849a7
Chris Lount <chris.lount@warerebel.com>
2019-09-01 20:02:14 +0100

    Re-write and stream support

--- README.md
+++ README.md
@@ -5,25 +5,106 @@
 # Intorduction
 Module to ease the storing of data in azure gen2 blob storage
 
-# Warning
-This module is a very early release and not ready for use.
-
 # Getting Started
 Call the constructor with the storage account name and the shared key for the storage resource.
 
-Store content to the blob storage account calling `storeFile`
 
 ```javascript
 const azureBlobStorage = require("azureblobstoragehelper");
 
 let myStorageHelper = new azureBlobStorage("account name", "Shared key");
+```
 
-myStorageHelper.storeFile("filename", "filesystem", "content", (error, response) => {
-    if(!error && response.statusCode < 400)
-        console.log("stored file");
+## Create a filesystem / container:
+
+As a convenience the createFilesystem function will not try to create a filesystem it already created, so you can call it without checking if you already created this filesystem. To skip this check and force the creation of the filesystem you should set the force option to true.
+
+```javascript
+let options = {
+    filesystem: "name", // Required - must be lower case
+    force: false // force filesystem creation - can be omitted
+}
+
+myStorageHelper.createFilesystem(options, (error, response, content) => {
+    if(error){ console.error(error.message); }
 });
 ```
 
-A filesystem that doesn't exist will be created.
-The filename should include the full desired save path.
+## Create a file
+```javascript
+let options = {
+    filesystem: "name", // Required - must be lower case
+    filename: "myfile.txt", // Required
+    httpHeaders: {
+        // Any custom http headers to set - optional and can be omitted
+        "Content-Type": "text/plain"
+    }
+}
+
+myStorageHelper.createFile(options, (error, response, content) => {
+    if(error){ console.error(error.message); }
+});
+```
+
+## Write content to a file
+```javascript
+let options = {
+    filesystem: "name", // Required - must be lower case
+    filename: "myfile.txt", // Required
+    content: "My file content", // Required - any valid Node http content
+    position: 0, // Optional - the position in the blob to append data - defaults to zero
+    httpHeaders: {
+        // Any custom http headers to set - optional and can be omitted
+        "Content-Type": "text/plain"
+    }
+}
+
+myStorageHelper.writeContent(options, (error, response, content) => {
+    if(error){ console.error(error.message); }
+});
+```
+
+## Flush file content
+```javascript
+let options = {
+    filesystem: "name", // Required - must be lower case
+    filename: "myfile.txt", // Required
+    position: 15, // Required - the position in the blob to flush data
+    httpHeaders: {
+        // Any custom http headers to set - optional and can be omitted
+        "Content-Type": "text/plain"
+    }
+}
+
+myStorageHelper.flushContent(options, (error, response, content) => {
+    if(error){ console.error(error.message); }
+});
+
+```
+## Create a file from a stream
+Azure blob storage does not support chunked transfer. The helper module simulates a chunked transfer by enacting multiple append operations to a single blob whilst reading from a stream.
+After the stream content is written to the blob, (i.e. after the stream emits end) it will automatically be flushed with no need to call `flushContent`.
+```javascript
+let options = {
+    filesystem: "name", // Required - must be lower case
+    filename: "myfile.txt", // Required
+    httpHeaders: {
+        // Any custom http headers to set - optional and can be omitted
+        "Content-Type": "text/plain"
+    }
+}
+
+const fs = require("fs");
+let instream = fs.createReadStream("./myfile.txt");
+
+myStorageHelper.writeStream(options, instream,  (error, response, content) => {
+    if(error){ console.error(error.message); }
+});
+
+```
+# Notes
+- A filesystem that doesn't exist will be created.
+- The filename should include the full desired save path.
+- A user set "x-ms-version" header is ignored and will be overwritten to version "2019-02-02".
+- A user set "x-ms-date" heaeder is ignored and set to current system time.
 
--- test/azureBlobStorage.spec.js
+++ test/azureBlobStorage.spec.js
@@ -1,152 +1,139 @@
 /* eslint-disable no-undef */
 const sinon = require("sinon");
-const https = require("https");
 const assert = require("assert");
 const passthrough = require("stream").PassThrough;
 const azureBlobStorage = require("../lib/azureBlobStorage");
 
-describe("It receives a filesystem, a filename and content and stores the content over an HTTP rest api", function(){
+describe("It provides a convenience wrapper around the Azure blob storage rest api", function(){
 
-    before(function(){
-        this.callbacks = [];
-        this.request = sinon.stub(https, "request");
-        this.request.on = function(name, callback){this.callbacks[name] = callback;}.bind(this);
-        this.request.end = function(){};
-        this.request.write = function(){};
+    this.beforeEach(function(){
+        this.myAzureBlobStorage = new azureBlobStorage("account", "key");
     });
 
-    after(function(){
-        sinon.restore();
-    });
-
-    beforeEach(function(){
-        this.myAzureBlobStorage = new azureBlobStorage("testAccount", "12345");
-    });
-
-    it("Executes an http call with received options and returns the result", function(done){
-        let content = "Test returned content";
-        let response = new passthrough();
-        response.statusCode = 200;
-        response.write(JSON.stringify(content));
-        this.request.callsArgWith(1, response).returns(this.request);
-
+    it("Checks if a filesystem exists and tries to create it if not", function(done){
+        let executeStub = sinon.stub(this.myAzureBlobStorage, "executeRequest").yields(null, {statusCode: 200});
         let options = {
-            method: "PUT",
-            protocol: "https:",
-            host: "testhost.dfs.core.windows.net",
-            path: "/test?resource=filesystem",
-            headers: {
-                "x-ms-date": new Date().toUTCString(),
-                "x-ms-version": "2019-02-02"
-            }
+            filesystem: "myfs"
         };
-
-        this.myAzureBlobStorage.executeRequest(options, "Test sent content", (error, result) => {
-            assert(!error);
-            assert.deepEqual(result.statusCode, 200);
-            done();
-        });
-        response.end();
-    });
-
-    it("Executes an http call with received options and handles an error", function(done){
-        let content = "Test returned content";
-        let response = new passthrough();
-        response.statusCode = 200;
-        response.write(JSON.stringify(content));
-        this.request.callsArgWith(1, response).returns(this.request);
-
-        let options = {
-            method: "PUT",
-            protocol: "https:",
-            host: "testhost.dfs.core.windows.net",
-            path: "/test?resource=filesystem",
-            headers: {
-                "x-ms-date": new Date().toUTCString(),
-                "x-ms-version": "2019-02-02"
-            }
-        };
-
-        this.myAzureBlobStorage.executeRequest(options, "Test sent content", (error, response) => {
-            assert.deepEqual(error.message, "error");
-            assert(!response);
-            done();
-        });
-        response.emit("error", {message: "error"});
-    });
-
-    it("Checks for a remote filesystem only once and uses it's cache on subsequent checks", function(done){
-        let executeStub = sinon.stub(this.myAzureBlobStorage, "executeRequest").yieldsRight(null, {statusCode: 200});
-        let filesystem = "myfilesystem";
-        this.myAzureBlobStorage.checkFilesystem(filesystem, (error, result) => {
-            assert(!error);
-            assert.deepEqual(result.statusCode, 200);
+        this.myAzureBlobStorage.createFilesystem(options, () => {
             assert.deepEqual(executeStub.callCount, 1);
-            this.myAzureBlobStorage.checkFilesystem(filesystem, (error, result) => {
-                assert(!error);
-                assert.deepEqual(result.statusCode, 200);
+            this.myAzureBlobStorage.createFilesystem(options, () => {
                 assert.deepEqual(executeStub.callCount, 1);
                 done();
             });
         });
     });
 
-    it("Creates, writes and flushes a file", function(done){
+    it("creates a file in blob storage", function(done){
         let executeStub = sinon.stub(this.myAzureBlobStorage, "executeRequest").yields(null, {statusCode: 200});
-        let filesystem = "myfilesystem";
-        let filename = "myfilename";
-        let content = "mycontent";
-        this.myAzureBlobStorage.createFile(filename, filesystem, content, (error, result) => {
-            assert(!error);
-            assert.deepEqual(result.statusCode, 200);
-            assert.deepEqual(executeStub.callCount, 3);
+        let timestamp = new Date().toUTCString();
+        let options = {
+            filesystem: "name",
+            filename: "myfile.txt",
+            httpHeaders: {
+                "Content-Type": "text/plain"
+            }
+        };
+        let expectedOptions = {
+            method: "PUT",
+            protocol: "https:",
+            host: "account.dfs.core.windows.net",
+            path: "/name/myfile.txt?resource=file",
+            headers: {
+                "x-ms-date": timestamp,
+                "x-ms-version": "2019-02-02",
+                "Content-Length": 0,
+                "Content-Type": "text/plain"
+            }
+        };
+        
+        this.myAzureBlobStorage.createFile(options, () => {
+            assert.deepEqual(executeStub.args[0][0], expectedOptions);
             done();
         });
     });
 
-    it("Stops file creation when one of the steps fails", function(done){
-        let filesystem = "myfilesystem";
-        let filename = "myfilename";
-        let content = "mycontent";
-        let executeStub = sinon.stub(this.myAzureBlobStorage, "executeRequest");
-        executeStub.onFirstCall().yields(null, {statusCode: 200});
-        executeStub.onSecondCall().yields(null, {statusCode: 400});
-        this.myAzureBlobStorage.createFile(filename, filesystem, content, (error, result) => {
-            assert(!error);
-            assert.deepEqual(result.statusCode, 400);
-            assert.deepEqual(executeStub.callCount, 2);
+    it("appends fixed data to a file in blob storage", function(done){
+        let executeStub = sinon.stub(this.myAzureBlobStorage, "executeRequest").yields(null, {statusCode: 200});
+        let timestamp = new Date().toUTCString();
+        let options = {
+            filesystem: "name",
+            filename: "myfile.txt",
+            content: "My Content",
+            httpHeaders: {
+                "Content-Type": "text/plain"
+            }
+        };
+        let expectedOptions = {
+            method: "PATCH",
+            protocol: "https:",
+            host: "account.dfs.core.windows.net",
+            path: "/name/myfile.txt?action=append&position=0",
+            headers: {
+                "x-ms-date": timestamp,
+                "x-ms-version": "2019-02-02",
+                "Content-Length": 10,
+                "Content-Type": "text/plain"
+            }
+        };
+        
+        this.myAzureBlobStorage.writeContent(options, () => {
+            assert.deepEqual(executeStub.args[0][0], expectedOptions);
             done();
         });
     });
 
-    it("Stores a file in blob storage", function(done){
-        let filesystem = "myfilesystem";
-        let filename = "myfilename";
-        let content = "mycontent";
-        let createFileStub = sinon.stub(this.myAzureBlobStorage, "createFile").yields(null, {statusCode: 200});
-        let checkfileSystemStub = sinon.stub(this.myAzureBlobStorage, "checkFilesystem").yields(null, {statusCode: 200});
-        this.myAzureBlobStorage.storeFile(filename, filesystem, content, (error, result) => {
-            assert.deepEqual(result.statusCode, 200);
-            assert.deepEqual(createFileStub.callCount, 1);
-            assert.deepEqual(checkfileSystemStub.callCount, 1);
-            assert(!error);
-            done();
+    it("pipes data to a blob", function(done){
+        let instream = new passthrough();
+        let executeStub = sinon.stub(this.myAzureBlobStorage, "executeRequest").yields(null, {statusCode: 200});
+        let options = {
+            filesystem: "name", // Required - must be lower case
+            filename: "myfile.txt", // Required
+            httpHeaders: {
+                // Any custom http headers to set - optional and can be omitted
+                "Content-Type": "text/plain"
+            }
+        };
+        instream.write("Data", "utf8", () => {
+            instream.write("Data", "utf8", () => {
+                this.myAzureBlobStorage.writeStream(options, instream,  () => {
+                    assert.deepEqual(executeStub.callCount, 3);
+                    assert.deepEqual(executeStub.args[2][0].path, "/name/myfile.txt?action=flush&position=8");
+                    done();
+                });
+                instream.end(); 
+            });
         });
     });
-
-    it("handles error flow correctly during requests", function(done){
-        let content = "Test returned content";
-        let response = new passthrough();
-        response.statusCode = 400;
-        response.write(JSON.stringify(content));
-        this.request.callsArgWith(1, response).returns(this.request);
-
-        this.myAzureBlobStorage.createFile("myfilename", "myfilesystem", null, (error, result) => {
-            assert(!error);
-            assert.deepEqual(result.statusCode, 400);
+    
+    it("flushes data to a file in blob storage", function(done){
+        let executeStub = sinon.stub(this.myAzureBlobStorage, "executeRequest").yields(null, {statusCode: 200});
+        let timestamp = new Date().toUTCString();
+        let options = {
+            filesystem: "name",
+            filename: "myfile.txt",
+            position: 10,
+            httpHeaders: {
+                "Content-Type": "text/plain"
+            }
+        };
+        let expectedOptions = {
+            method: "PATCH",
+            protocol: "https:",
+            host: "account.dfs.core.windows.net",
+            path: "/name/myfile.txt?action=flush&position=10",
+            headers: {
+                "x-ms-date": timestamp,
+                "x-ms-version": "2019-02-02",
+                "Content-Length": 0,
+                "Content-Type": "text/plain"
+            }
+        };
+        
+        this.myAzureBlobStorage.flushContent(options, () => {
+            assert.deepEqual(executeStub.args[0][0], expectedOptions);
             done();
         });
-        response.end();
     });
 
 });
